2c4bc2f8f833f58d383b506ee3a7e5e340a9bce4
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-04-20 00:16:50 +0900

    :white_check_mark: test: Redis Test

---
+++ src/main/java/com/baby/monitor/RedisRunner.java
@@ -0,0 +1,19 @@
+package com.baby.monitor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisRunner implements ApplicationRunner {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
