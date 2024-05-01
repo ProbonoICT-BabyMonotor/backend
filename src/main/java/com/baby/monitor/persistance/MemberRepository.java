ef10c80ac775b0c336a916a45f59a3bb7aead338
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-05-01 20:56:56 +0900

    :sparkles: feat: 회원가입 로직 구현

--- src/main/java/com/baby/monitor/persistance/MemberRepository.java
+++ src/main/java/com/baby/monitor/persistance/MemberRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository {
     MemberVO save(MemberVO member);
+
+    MemberVO findByMemberId(String memberId);
 }
--- src/main/java/com/baby/monitor/service/MemberService.java
+++ src/main/java/com/baby/monitor/service/MemberService.java
@@ -1,4 +1,40 @@
 package com.baby.monitor.service;
 
+import com.baby.monitor.domain.MemberVO;
+import com.baby.monitor.persistance.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /***
+     * 회원 가입하는 메소드 [비밀번호 인코딩 ]
+     * @param member
+     * @return member (회원가입한 친구들)
+     */
+    public MemberVO signUpMember(MemberVO member) throws Exception {
+        String inputPW = member.getMemberPassword();
+        member.setMemberPassword(passwordEncoder.encode(inputPW));
+
+        // 아이디 중복이면 오류 발생, 아니면 회원가입 진행
+        if(memberRepository.findByMemberId(member.getMemberId()) != null){
+            throw new IllegalStateException("이미 가입된 아이디가 있습니다.");
+        }
+        else{
+            return memberRepository.save(member);
+        }
+    }
 }
